93cb99283c7adaa39afb646d4f043d75841d2c08
clasesucatmarlon <clasesucatmarlon@gmail.com>
2022-05-05 20:08:15 -0500

    Modificados nombre de variables y ajustados algunos estilos

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
-const textEncritarDesencriptar = document.querySelector("#textEncritarDesencriptar");
-const textEncriptadoDesencriptado = document.querySelector("#textEncriptadoDesencriptado");
+const txtEncryptDecrypt = document.querySelector("#txtEncryptDecrypt");
+const txtEncryptionDecrypted = document.querySelector("#txtEncryptionDecrypted");
 
-function btnEncriptar () {
-  const txtEncriptado = Encriptar(textEncritarDesencriptar.value);
-  textEncriptadoDesencriptado.value = txtEncriptado;
+// *************************************************************************************
+// FUNCTIONALITY TO ENCRYPT A TEXT OR TEXT STRING
+// *************************************************************************************
+function btnEncrypt () {
+  const txtEncryption = Encrypt(txtEncryptDecrypt.value);
+  txtEncryptionDecrypted.value = txtEncryption;
+  txtEncryptDecrypt.value="";
+}
+
+function Encrypt (txtEncryptDecrypt) {
+  let arrayEncrypt = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  txtEncryptDecrypt = txtEncryptDecrypt.toLowerCase();
+  for(let i = 0; i < arrayEncrypt.length; i++ ) {
+    if (txtEncryptDecrypt.includes(arrayEncrypt[i][0])) {
+      txtEncryptDecrypt = txtEncryptDecrypt.replaceAll(arrayEncrypt[i][0], arrayEncrypt[i][1]);
+    }
+  }
+  return txtEncryptDecrypt;
 }
 
 
+// *************************************************************************************
+// FUNCTIONALITY TO DECRYPT A TEXT OR TEXT STRING
+// *************************************************************************************
+function btnDecrypt () {
+  const txtDecrypted= Decrypt(txtEncryptDecrypt.value);
+  txtEncryptionDecrypted.value = txtDecrypted;
+}
 
-function Encriptar (textEncritarDesencriptar) {
-  let arrayEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-  textEncritarDesencriptar = textEncritarDesencriptar.toLowerCase();
-  for(let i = 0; i < arrayEncriptacion.length; i++ ) {
-    if (textEncritarDesencriptar.includes(arrayEncriptacion[i][0])) {
-      textEncritarDesencriptar = textEncritarDesencriptar.replaceAll(arrayEncriptacion[i][0], arrayEncriptacion[i][1]);
+function Decrypt(txtEncryptDecrypt) {
+  let arrayDecrypt = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+  
+  for(let i = 0; i < arrayDecrypt.length; i++ ) {
+    if (txtEncryptDecrypt.includes(arrayDecrypt[i][0])) {
+      txtEncryptDecrypt = txtEncryptDecrypt.replaceAll(arrayDecrypt[i][0], arrayDecrypt[i][1]);
     }
   }
-  return textEncritarDesencriptar
+  return txtEncryptDecrypt;
+}
+
+
+// *************************************************************************************
+// FUNCTIONALITY TO COPY A TEXT STRING TO THE CLIPBOARD
+// *************************************************************************************
+function copyToClipBoard() {
+  let content = document.querySelector('#txtEncryptionDecrypted');
+  content.select();
+  document.execCommand('copy');
 }
